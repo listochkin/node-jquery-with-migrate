68af184f73f3c32ee4f50155e2e9347adf2cb4d1
Андрей Листочкин <andrew.listochkin@gmail.com>
2013-03-26 17:28:20 +0200

    Ajax test

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+/*jshint unused:false*/
 /*global require, console*/
 'use strict';
 var $ = require('./index');
@@ -5,6 +6,17 @@ var $ = require('./index');
 $('<div id="test">').appendTo($('body'));
 
 if (!$('#test'))
-    throw 'Test failed';
+    throw 'DOM test failed';
 else
-    console.log('Test passed');
+    console.log('DOM test passed');
+
+require('jsdom').env('https://google.com', function (error, window) {
+    if (error) return;
+    $.create(window).ajax({
+        url: 'https://api.github.com/repos/joyent/node'
+    }).done(function () {
+        console.log('Ajax test passed');
+    }).fail(function (xhr, status, message) {
+        throw new Error('Ajax test failer with (' + status + ') ' + message);
+    });
+});
